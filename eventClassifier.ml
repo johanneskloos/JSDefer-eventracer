a8341e53e3cd201c1e301ea23122106e853f5cfb
jkloos <jkloos@mpi-sws.org>
2016-11-02 16:50:42 +0100

    unused

--- eventClassifier.ml
+++
@@ -1,8 +0,0 @@
-open Trace
-
-let is_page_onload { commands } =
-  (* Checking for the load event is tricky - it can get fired by other
-     elements as well. But pageshow is window-only, and webkit fires
-     them both in one go. *)
-  BatList.mem (Enter (Event "showpage")) commands
-
