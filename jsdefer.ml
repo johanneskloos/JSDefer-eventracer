e8a740aae3dda5c1ca06aff10e38aedc643ebf5e
jkloos <jkloos@mpi-sws.org>
2016-11-10 18:24:21 +0100

    Provide timeouts

--- jsdefer.ml
+++ jsdefer.ml
@@ -1,12 +1,21 @@
 let task_pool_size = ref 0
 let task_pool_max = ref 4
 
-let start_task f x =
+let start_task timeout f x =
   while !task_pool_size >= !task_pool_max do
     ignore (Unix.wait ())
   done;
   match Unix.fork () with
-    | 0 -> f x; exit 0
+    | 0 ->
+        begin
+          match timeout with
+            | Some t ->
+                Sys.set_signal Sys.sigalrm (Sys.Signal_handle exit);
+                ignore (Unix.alarm t)
+            | None -> ()
+        end;
+        f x;
+        exit 0
     | pid -> incr task_pool_size
 
 let drain () =
@@ -18,12 +27,15 @@ let chldhandler (_: int) = decr task_pool_size
 
 let () =
   let log = ref false
-  and tasks = ref [] in
+  and tasks = ref []
+  and timeout = ref None in
   Arg.parse [
     ("-G", Arg.Set OrderGraph.guid_heuristic, "GUID heuristic (HACK)");
     ("-L", Arg.Set log, "log file");
-    ("-t", Arg.Set_int task_pool_max, "number of parallel tasks")
+    ("-n", Arg.Set_int task_pool_max, "number of parallel tasks");
+    ("-t", Arg.Int (fun n -> timeout := Some n), "timeout (in seconds)");
+    ("-T", Arg.Unit (fun () -> timeout := None), "no timeout")
   ] (fun task -> tasks := task :: !tasks) "";
   Sys.set_signal Sys.sigchld (Sys.Signal_handle chldhandler);
-  List.iter (fun fn -> start_task (JsdeferCommon.analyze log) fn) !tasks;
+  List.iter (fun fn -> start_task !timeout (JsdeferCommon.analyze log) fn) !tasks;
   drain ()
