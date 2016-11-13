8c6bc1e116cb95c20b2450d75890eadd4f130283
jkloos <jkloos@mpi-sws.org>
2016-11-13 20:07:28 +0100

    Write machine-readable log file

--- jsdefer.ml
+++ jsdefer.ml
@@ -30,13 +30,23 @@ let run_analysis log file =
   JsdeferCommon.analyze log file
 
 let improved_reporter () =
-  let report (src: Logs.src) (level: Logs.level) ~over k msgf =
-    let k _ = over (); k () in
-      msgf @@ fun ?header ?tags fmt ->
-      Format.kfprintf k Fmt.stdout ("%a %s: @[" ^^ fmt ^^ "@]@.")
-        Logs.pp_header (level, header)
-        (Logs.Src.name src)
-  in { Logs.report }
+  let logfile =
+    open_out "log.txt"
+  in at_exit (fun () -> close_out logfile);
+     let report (src: Logs.src) (level: Logs.level) ~over k msgf =
+       let k _ = over (); k () in
+         msgf @@ fun ?header ?tags fmt ->
+         Format.kasprintf (fun txt ->
+                            output_string logfile 
+                              (Logs.level_to_string (Some level) ^ ":" ^
+                               Logs.Src.name src ^ ":" ^
+                               BatString.quote txt ^ "\n");
+                            Format.kfprintf k Fmt.stdout ("%a %s: @[%s@]@.")
+                              Logs.pp_header (level, header)
+                              (Logs.Src.name src)
+                              txt)
+           fmt
+     in { Logs.report }
 
 let () =
   Logs.set_reporter (improved_reporter ());
