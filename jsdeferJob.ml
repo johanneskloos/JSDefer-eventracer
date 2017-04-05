ba73d8a03235a2db3d2a9fb4c33dfeb770677fb7
jkloos <jkloos@mpi-sws.org>
2017-04-05 18:59:15 +0200

    Removed bit-rotted and unused files

--- jsdeferJob.ml
+++
@@ -1,6 +0,0 @@
-let () =
-  let log = ref false in
-  Arg.parse [
-    ("-G", Arg.Set OrderGraph.guid_heuristic, "GUID heuristic (HACK)");
-    ("-L", Arg.Set log, "log file")
-  ] (JsdeferCommon.analyze log) ""
