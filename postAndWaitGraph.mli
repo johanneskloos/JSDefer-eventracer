5b02051db6c874f60f7f28c45e94f0a942b33f53
jkloos <jkloos@mpi-sws.org>
2017-04-05 19:14:19 +0200

    Add interface for PostAndWaitGraph

---
+++ postAndWaitGraph.mli
@@ -0,0 +1,6 @@
+module PostWaitEdge : sig type t = POST | HB [@@deriving ord] end
+module PostWaitGraph : Graph.Sig.P with
+        type V.t = int
+        and type E.t = int * PostWaitEdge.t * int
+val build_post_wait_graph :
+  Trace.trace -> ClassifyTask.classification IntMap.t -> PostWaitGraph.t
