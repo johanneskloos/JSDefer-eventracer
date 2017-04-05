ac1efb6275d8a53540b4cc0c0624c07e4752394b
jkloos <jkloos@mpi-sws.org>
2017-04-05 19:35:50 +0200

    Add interface to Domination

---
+++ domination.mli
@@ -0,0 +1,31 @@
+type analysis_result = {
+  dom_accesses : IntSet.t;
+  inline_scripts : IntSet.t;
+  async_scripts : IntSet.t;
+  nondet : StringSet.t;
+}
+val pp_analysis_result : analysis_result Fmt.t
+
+type verdict =
+    Deferable
+  | Deferred
+  | HasDOMAccess
+  | IsInlineScript
+  | IsAsyncScript
+  | DominatedByDOMAccess
+  | DominatedByInlineScript
+  | DominatedByAsyncScript
+  | Nondeterministic
+val verdict_to_string : verdict -> string
+val pp_verdict : verdict Fmt.t
+
+type result = { verdict : verdict; nondet : bool; data : analysis_result; }
+val pp_result : result Fmt.t
+
+val calculate_domination :
+  IntSet.t ->
+  Trace.trace ->
+  Trace.trace * ClassifyTask.classification IntMap.t *
+  ReducedOrderGraph.data * ReducedOrderGraph.data *
+  Trace.value option Trace.ReferenceMap.t * Trace.DependencyGraph.t *
+  (int -> analysis_result) * result IntMap.t
