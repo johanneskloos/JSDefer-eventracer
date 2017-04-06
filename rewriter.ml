01900e81ed288be580891fb3310b5f83d501751c
jkloos <jkloos@mpi-sws.org>
2017-04-06 20:06:59 +0200

    Make rewriter (kinda) work through gross gross hacks

--- rewriter.ml
+++ rewriter.ml
@@ -1,6 +1,25 @@
-let canonical_uri url =
+let re_dot = Str.regexp_string "."
+let totally_an_alias parts1 parts2 =
+  let open BatList in
+  let len = Pervasives.min (length parts1) (length parts2) in
+  let l1 = drop (length parts1 - len) parts1
+  and l2 = drop (length parts2 - len) parts2
+  in l1 = l2 || (len > 2 && tl l1 = tl l2)
+
+let canonical_uri base url =
   let open Uri in
-    with_scheme (of_string url) (Some "http") |> canonicalize
+  let url = of_string url in
+  let url = match host url with
+    | Some host ->
+        (* GROSS HACK *)
+        let parts_host = Str.split re_dot host
+        and parts_base = Str.split re_dot base
+        in if totally_an_alias parts_host parts_base then
+          with_host url (Some base)
+        else url
+    | None ->
+        with_host url (Some base)
+  in with_scheme url (Some "http") |> canonicalize
 
 let read_tasks base =
   let csv = Csv.load (Filename.concat base "result.csv") in
@@ -11,19 +30,16 @@ let read_tasks base =
          | _ -> None)
       csv
     |> BatList.sort (fun (id1, _) (id2, _) -> compare id1 id2)
-    |> BatList.map (fun (_, (url, defer)) -> (canonical_uri url, defer))
+    |> BatList.map (fun (_, (url, defer)) -> (canonical_uri base url, defer))
 
-let resolve_uri base url =
-  let open Uri in resolve "http" base (of_string url) |> canonicalize
-
-let extract_sync_scripts base_url scripts =
+let extract_sync_scripts base scripts =
   let open Soup in
     scripts
     |> filter (fun node -> has_attribute "src" node &&
                            not (has_attribute "async" node) &&
                            not (has_attribute "defer" node))
     |> fold (fun scripts node -> (R.attribute "src" node, node) :: scripts) []
-    |> BatList.rev_map (fun (url, node) -> (resolve_uri base_url url, node))
+    |> BatList.rev_map (fun (url, node) -> (canonical_uri base url, node))
 
 let merge_tasks l1 l2 =
   let open Fmt in
@@ -38,20 +54,21 @@ let merge_tasks l1 l2 =
 let read_document base =
   let open Soup in
   let document = read_file (Filename.concat base "index.html") |> parse
-  in let scripts = begin document $$ "script"
-    |> extract_sync_scripts (Uri.of_string (Filename.basename base))
-    end
+  in let scripts = document $$ "script" |> extract_sync_scripts base
   in (scripts, document)
 
 let process base =
   let tasks = read_tasks base
   and (scripts, document) = read_document base
   in let shared_scripts = merge_tasks tasks scripts
-  in BatList.iter (function
-                     | (_, true, node) -> Soup.set_attribute "defer" "" node
-                     | _ -> ())
-       shared_scripts;
-     let outfile = Filename.concat base "index.defer.html" in
-     Soup.to_string document |> Soup.write_file outfile
+  in 
+    Soup.to_string document
+    |> Soup.write_file (Filename.concat base "index.nodefer.html");
+    BatList.iter (function
+                    | (_, true, node) -> Soup.set_attribute "defer" "" node
+                    | _ -> ())
+      shared_scripts;
+    let outfile = Filename.concat base "index.defer.html" in
+      Soup.to_string document |> Soup.write_file outfile
 
 let () = Arg.parse [] process "rewrite dirs"
