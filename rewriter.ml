709ecdb2730d187e2523f9d4d06a656599bbf080
jkloos <jkloos@mpi-sws.org>
2017-04-06 19:37:26 +0200

    Only rewrite scripts that appear in LCS

--- rewriter.ml
+++ rewriter.ml
@@ -1,5 +1,3 @@
-type task = { url: Uri.t; defer: bool }
-
 let canonical_uri url =
   let open Uri in
     with_scheme (of_string url) (Some "http") |> canonicalize
@@ -13,7 +11,7 @@ let read_tasks base =
          | _ -> None)
       csv
     |> BatList.sort (fun (id1, _) (id2, _) -> compare id1 id2)
-    |> BatList.map (fun (_, (url, defer)) -> { url = canonical_uri url; defer })
+    |> BatList.map (fun (_, (url, defer)) -> (canonical_uri url, defer))
 
 let resolve_uri base url =
   let open Uri in resolve "http" base (of_string url) |> canonicalize
@@ -27,27 +25,28 @@ let extract_sync_scripts base_url scripts =
     |> fold (fun scripts node -> (R.attribute "src" node, node) :: scripts) []
     |> BatList.rev_map (fun (url, node) -> (resolve_uri base_url url, node))
 
-let rec apply_tasks tasks reals =
-  match tasks, reals with
-    | { url=url1; defer } :: tasks, (url2, node)::reals when url1 = url2 ->
-        if defer then Soup.set_attribute "defer" "" node;
-        apply_tasks tasks reals
-    | _ :: tasks, reals -> apply_tasks tasks reals
-    | [], [] -> ()
-    | [], scripts ->
-        failwith @@
-        let open Fmt in
-        strf "@[<v>Left-over scripts:@,%a@]"
-          (list ~sep:cut (using fst Uri.pp_hum)) scripts
+let merge_tasks =
+  LongestCommonSubsequence.longest_common_subsequence
+    (fun (url1, _) (url2, _) -> Uri.equal url1 url2)
+    (fun (url, defer) (_, node) -> (url, defer, node))
+
+let read_document base =
+  let open Soup in
+  let document = read_file (Filename.concat base "index.html") |> parse
+  in let scripts = begin document $$ "script"
+    |> extract_sync_scripts (Uri.of_string (Filename.basename base))
+    end
+  in (scripts, document)
 
 let process base =
-  let open Soup in
   let tasks = read_tasks base
-  and document = read_file (Filename.concat base "index.html") |> parse in
-    begin document $$ "script"
-    |> extract_sync_scripts (Uri.of_string (Filename.basename base))
-    |> apply_tasks tasks
-    end;
-    to_string document |> write_file (Filename.concat base "index.deferred.html")
+  and (scripts, document) = read_document base
+  in let shared_scripts = merge_tasks tasks scripts
+  in BatList.iter (function
+                     | (_, true, node) -> Soup.set_attribute "defer" "" node
+                     | _ -> ())
+       shared_scripts;
+     let outfile = Filename.concat base "index.defer.html" in
+     Soup.to_string document |> Soup.write_file outfile
 
-let () = Arg.parse [] process "rewrite directories"
+let () = Arg.parse [] process "rewrite dirs"
